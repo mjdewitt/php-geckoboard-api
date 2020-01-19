30f366496a758c360e0ecf49e0e95a604b4d1f46
Michael DeWitt <36002368+mjdewitt@users.noreply.github.com>
2020-01-19 14:56:46 -0500

    Update Entry.php
    extended implementation to include series type, series incomplete, date handling, and scatter type series.

--- src/CarlosIO/Geckoboard/Data/LineChart2/Entry.php
+++ src/CarlosIO/Geckoboard/Data/LineChart2/Entry.php
@@ -42,6 +42,14 @@ class Entry
     protected $seriesName;        
     /**
      * @var string
+     */
+    protected $seriesType;            
+    /**
+     * @var string
+     */
+    protected $seriesIncomplete;                
+    /**
+     * @var string
      */    
     protected $seriesValue;    
       
@@ -52,9 +60,12 @@ class Entry
         $this->format = null;
         $this->unit = null; 
         $this->xAxisName = null; 
+        $this ->xAxisType = null;
         $this->xAxisLabels = array();
         $this->seriesName = null;
+        $this->seriesIncomplete = null;
         $this->seriesData = array();
+        $this->seriesValue = null;
                       
     }
 
@@ -75,10 +86,73 @@ class Entry
      *
      * @return $this
      */
-    public function addSeriesValue($seriesName,$seriesValue)
+    public function setSeriesType($seriesType)
     {
-        $this->seriesData[$seriesName][] = $seriesValue;
+    	if ($seriesType != 'datetime') $seriesType = null;
+        $this->seriesData['type'] = $seriesType;
 
+        return $this;
+    }     
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setSeriesIncomplete($seriesIncomplete)
+    {
+    	if ($this->seriesData['type'] == 'datetime') {
+        $this->seriesData['incomplete'] = $seriesIncomplete;
+      } else {
+      	$this->seriesData['incomplete'] = null;;
+      }
+
+        return $this;
+    }         
+    
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function addSeriesValue($seriesName,$seriesValue1,$seriesValue2=NULL)
+    {
+//    	$valuesArray=implode(',',$seriesValue);
+//    	$values = $valuesArray[1];
+//echo '<p><pre>';
+//var_dump($seriesValue1);
+//echo '<br>';
+//var_dump($seriesValue2);
+//echo '</pre></p>';
+ //   	if (count($values) > 2 ) {
+ //   		    	throw new \Exception('Series data has too many entries');
+ //   	}
+    	
+    		if ($seriesValue1 != null) {
+    			if (! is_numeric($seriesValue1) && 
+    					(! strtotime($seriesValue1) && 
+    					isset( $this->seriesData[$seriesName]['type'] ) && 
+    					$this->seriesData[$seriesName]['type'] != 'datetime' ) ) {
+    				throw new \Exception('Series X value is not numeric or date');
+    			}
+    			//echo '<br> is value1 date '.strtotime($seriesValue1);
+    			if ( (stristr('-',$seriesValue1) || stristr('/',$seriesValue1)) && strtotime($seriesValue1)  &&  (! isset( $this->seriesData[$seriesName]['type'] ) || $this->seriesData[$seriesName]['type'] != 'datetime') ) {
+    				throw new \Exception(' Series is not type datetime and date included in data');
+    			}    			
+    			if ((stristr('-',$seriesValue1) || stristr('/',$seriesValue1)) &&  strtotime($seriesValue1) && count( $this->xAxisLabels ) ) {
+    				throw new \Exception('Series X labels already defined');
+    			}    			    			
+				}
+    		if ($seriesValue2 != null) {				
+      		if (! is_numeric($seriesValue2)  ) {
+    				throw new \Exception('Series Y value is not numeric');
+    			}  	
+    	}
+if ($seriesValue2 != null ) {
+        $this->seriesData[$seriesName][] = array($seriesValue1,$seriesValue2);
+} else {
+	        $this->seriesData[$seriesName][] = $seriesValue1;
+}
         return $this;
     }         
     /**
@@ -119,7 +193,9 @@ class Entry
      */
     public function setLabelsxAxis($label)
     {
-        $this->xAxisLabels[] = $label;
+    	    	if (isset($this->seriesData['type']) && $this->seriesData['type'] != 'datetime') {
+       $this->xAxisLabels[] = $label;
+     } 
 
         return $this;
     }    
@@ -155,7 +231,28 @@ class Entry
         return $this->unit;
     }    
     
-
+    /**
+     * @return string
+     */
+    public function getSeriesType()
+    {
+    	if (isset($this->seriesData['type'])) {
+        return $this->seriesData['type'];
+      } else {
+      	return null;
+      }
+    }  
+    /**
+     * @return string
+     */
+    public function getSeriesIncomplete()
+    {
+    	  if (isset($this->seriesData['incomplete'])) {
+        return $this->seriesData['incomplete'];
+      } else {
+      	return null;
+      }
+    }      
         /**
      * @return array
      */
@@ -165,12 +262,19 @@ class Entry
     	$series = array();
     	$data = array();
     	$seriesDataArray = (array)$this->seriesData;
+    	unset($seriesDataArray['type']);
+    	unset($seriesDataArray['incomplete']);
+    	 
     	foreach($seriesDataArray as $key=>$value) {
-    		$savekey=$key;
+    		//echo '<br>key= '.$key;
+        if (is_string($key)) $name=$key;
+    		 if (is_array($value)) {
     		foreach($value as $key1=>$value1) {
+    			    	//	echo '<br>key1= '.$key1;
     			$data[]=$value1;
     		}
-    			$series[]=array('name'=>$key,'data'=>$data);
+    	}
+    			$series[]=array('name'=>$name,'data'=>$data);
     			$data=array();
     	}
 
@@ -195,12 +299,22 @@ class Entry
             'unit' => $this->unit
             ) ;
         }
-
-
+        if (null !== $this->getSeriesType() ) {
+            $result['x_axis'] = array(
+            'type'=>$this->getSeriesType()
+            ) ;
+        }
+        if (null !== $this->getSeriesIncomplete() ) {
+            $result['incomplete_from'] = array(
+            $this->getSeriesIncomplete()
+            ) ;
+        }
+        if (count($this->xAxisLabels)) {
         $xAxisLabels = $this->xAxisLabels;
 				foreach ($xAxisLabels as $key=>$data) {
 						$result['x_axis']['labels'][] = $data; 
 					}
+}
 					 $result['series']=$this->getSeriesData();
 
         return $result;
